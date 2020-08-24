04187cf27fd0acc17060eef1cc4b076a5d2d1701
Menghua <zoeaeen13@gmail.com>
2020-08-24 15:58:18 +0800

    adjust hw3

--- homeworks/week7/hw2/demo.js
+++ homeworks/week7/hw2/demo.js
@@ -1,7 +1,7 @@
 const windows = document.querySelector('html');
 windows.addEventListener('click', (e) => {
   if (e.target.classList.value.includes('faq')) {
-    const answer = e.target.closest('.faqItem').querySelector('.faqAnswer');
+    const answer = e.target.closest('.faq-item').querySelector('.faq-answer');
     answer.classList.toggle('hideAnswer');
   }
 });
