ab619343fcda6d55d5f07ad43e76f92781ecd913
Menghua <zoeaeen13@gmail.com>
2020-09-01 16:34:19 +0800

    refactor hw1/2

--- homeworks/week8/hw1/demo.js
+++ homeworks/week8/hw1/demo.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-alert,   prefer-destructuring */
 
 const request = new XMLHttpRequest();
+const url = 'https://dvwhnbka7d.execute-api.us-east-1.amazonaws.com/default/lottery';
 const lottery = document.querySelector('.lottery');
 const resultTitle = document.querySelector('.resultboard h1');
 const dashboard = document.querySelector('.dashboard');
 const resultboard = document.querySelector('.resultboard');
 const loading = document.querySelector('.loading');
+const errMsg = '系統不穩定，請再試一次';
 
 // hide dashboard
 function hideDashboard() {
@@ -22,52 +24,63 @@ function customView(title, imageLink) {
   hideDashboard();
 }
 
-// manage lucky draw result
-function setResult() {
-  loading.classList.toggle('hidden');
-
-  if (request.status >= 200 && request.status < 400) {
-    const response = request.responseText;
-    const prize = JSON.parse(response).prize;
-    switch (prize) {
-      case 'FIRST':
-        customView('恭喜你中頭獎了！日本東京來回雙人遊！', './first.jpg');
-        break;
-      case 'SECOND':
-        customView('獎！90 吋電視一台！', './second.jpg');
-        break;
-      case 'THIRD':
-        customView('恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！', './third.jpg');
-        break;
-      case 'NONE':
-        lottery.style.background = '#000000';
-        resultTitle.style.color = '#FFFFFF';
-        resultTitle.innerText = '銘謝惠顧';
-        hideDashboard();
-        break;
-      default:
-        alert('系統不穩定，請再試一次');
-        break;
-    }
-  } else {
-    alert('系統不穩定，請再試一次');
-  }
-}
-
 // call API
-function getLuckyDraw() {
-  request.open('GET', 'https://dvwhnbka7d.execute-api.us-east-1.amazonaws.com/default/lottery', true);
-  request.onload = setResult;
-  request.onError = () => {
-    console.log('error');
+function getLuckyDraw(callback) {
+  request.open('GET', url, true);
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 400) {
+      let data;
+      try {
+        data = JSON.parse(request.responseText);
+      } catch (error) {
+        callback(errMsg);
+        return;
+      }
+      if (!data.prize) {
+        callback(errMsg);
+        return;
+      }
+      callback(null, data);
+    } else {
+      callback(errMsg);
+    }
   };
+
+  request.onError = () => {
+    callback(errMsg);
+  };
+
   request.send();
 }
 
 // click button
 lottery.addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-lottery')) {
-    loading.classList.toggle('hidden');
-    getLuckyDraw();
+    loading.classList.remove('hidden');
+    getLuckyDraw((error, data) => {
+      loading.classList.add('hidden');
+      if (error) {
+        alert(error);
+        return;
+      }
+
+      switch (data.prize) {
+        case 'FIRST':
+          customView('恭喜你中頭獎了！日本東京來回雙人遊！', './first.jpg');
+          break;
+        case 'SECOND':
+          customView('獎！90 吋電視一台！', './second.jpg');
+          break;
+        case 'THIRD':
+          customView('恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！', './third.jpg');
+          break;
+        default:
+          lottery.style.background = '#000000';
+          resultTitle.style.color = '#FFFFFF';
+          resultTitle.innerText = '銘謝惠顧';
+          hideDashboard();
+          break;
+      }
+    });
   }
 });
--- homeworks/week8/hw2/demo.js
+++ homeworks/week8/hw2/demo.js
@@ -2,12 +2,24 @@
 
 const requestTopGame = new XMLHttpRequest();
 const requestStreams = new XMLHttpRequest();
+const url = 'https://api.twitch.tv/kraken';
+const template = `<img src="$banner" onerror="if (this.src != './default.jpg') this.src = './default.jpg';">
+<a href="$url"></a>
+<div class="itemInfo">
+  <div class="itemAvator">
+    <img src="$logo" onerror="if (this.src != 'error.jpg') this.src = './default.jpg';">
+  </div>
+  <div class="itemDetail">
+    <h3>$status</h3>
+    <p>$username</p>
+  </div>
+</div>`;
 const body = document.querySelector('body');
 const navbar = document.querySelector('.navbar');
 const gameList = document.querySelector('.gameList');
 const btnShowList = document.querySelector('.btn-show-list');
 const btnMore = document.querySelector('.btn-more');
-let recentGame = '';
+let recentGame;
 
 // add navbar items
 function setNavbar(arr) {
@@ -24,28 +36,40 @@ function setStreamItem(arr) {
   for (let i = 0; i < arr.length; i += 1) {
     const channel = arr[i].channel;
     const streamItem = document.createElement('div');
+    const content = template
+      .replace('$url', channel.url)
+      .replace('$banner', channel.video_banner)
+      .replace('$logo', channel.logo)
+      .replace('$status', channel.status)
+      .replace('$username', channel.name);
     streamItem.classList.add('streamItem');
-    streamItem.innerHTML = `<img src="${channel.video_banner}" onerror="if (this.src != './default.jpg') this.src = './default.jpg';">
-    <div class="itemInfo">
-      <div class="itemAvator">
-        <img src="${channel.logo}" onerror="if (this.src != 'error.jpg') this.src = './default.jpg';">
-      </div>
-      <div class="itemDetail">
-        <h3>${channel.status}</h3>
-        <p>${channel.name}</p>  
-      </div>
-    </div>`;
+    streamItem.innerHTML = content;
     gameList.appendChild(streamItem);
   }
 }
 
-// set streams
+function addEmptyItem() {
+  const emptyItem = document.createElement('div');
+  emptyItem.classList.add('streamItem-empty');
+  gameList.appendChild(emptyItem);
+}
+
+function removeEmptyItem() {
+  const elements = document.querySelectorAll('.streamItem-empty');
+  for (let i = 0; i < elements.length; i += 1) {
+    gameList.removeChild(elements[i]);
+  }
+}
+
+// call API and set streams
 function updateStreams(gameName) {
   requestStreams.onload = () => {
     if (requestStreams.status >= 200 && requestStreams.status < 400) {
       const response = JSON.parse(requestStreams.responseText);
       const streamList = response.streams;
       setStreamItem(streamList);
+      addEmptyItem();
+      addEmptyItem();
     } else {
       console.log('error');
     }
@@ -55,9 +79,8 @@ function updateStreams(gameName) {
     console.log('error');
   };
 
-  // call api: 20 items and set the offset
   const offset = gameList.childElementCount;
-  requestStreams.open('GET', `https://api.twitch.tv/kraken/streams?game=${gameName}&limit=20&offset=${offset}`, true);
+  requestStreams.open('GET', `${url}/streams?game=${gameName}&limit=20&offset=${offset}`, true);
   requestStreams.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
   requestStreams.setRequestHeader('Client-ID', '0n4il3nmibawzxq23hqdbid338v15p');
   requestStreams.send();
@@ -70,6 +93,7 @@ function getTopGame() {
       const response = JSON.parse(requestTopGame.responseText);
       const games = response.top;
       setNavbar(games);
+      // select the first item
       document.querySelector('.navItem').classList.add('select');
       recentGame = document.querySelector('.navItem').innerText;
       updateStreams(recentGame);
@@ -82,7 +106,7 @@ function getTopGame() {
     console.log('error');
   };
 
-  requestTopGame.open('GET', 'https://api.twitch.tv/kraken/games/top?limit=5', true);
+  requestTopGame.open('GET', `${url}/games/top?limit=5`, true);
   requestTopGame.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
   requestTopGame.setRequestHeader('Client-ID', '0n4il3nmibawzxq23hqdbid338v15p');
   requestTopGame.send();
@@ -90,30 +114,37 @@ function getTopGame() {
 
 getTopGame();
 
-// click showlist button
+// click
 btnShowList.addEventListener('click', () => {
   gameList.scrollIntoView();
 });
 
-// click more button
 btnMore.addEventListener('click', () => {
+  removeEmptyItem();
   updateStreams(recentGame);
 });
 
 body.addEventListener('click', (e) => {
-  // click navbar
+  // navbar
   if (e.target.closest('.navItem')) {
     recentGame = e.target.closest('.navItem').innerText;
     gameList.innerHTML = '';
     updateStreams(recentGame);
+
+    // update the recent navbar
+    const navItems = document.querySelectorAll('.navItem');
+    for (let i = 0; i < navItems.length; i += 1) {
+      if (navItems[i] === e.target) {
+        navItems[i].classList.add('select');
+      } else if (e.target.classList.contains('navItem')) {
+        navItems[i].classList.remove('select');
+      }
+    }
   }
 
-  const navItems = document.querySelectorAll('.navItem');
-  for (let i = 0; i < navItems.length; i += 1) {
-    if (navItems[i] === e.target) {
-      navItems[i].classList.add('select');
-    } else if (e.target.classList.contains('navItem')) {
-      navItems[i].classList.remove('select');
-    }
+  // item
+  if (e.target.closest('.streamItem')) {
+    const streamUrl = e.target.closest('.streamItem').querySelector('a').href;
+    window.location.href = streamUrl;
   }
 });
